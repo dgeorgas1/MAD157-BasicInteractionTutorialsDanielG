190a5f730d56d4a14ee648d235da9227bc102afe
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-02-04 22:24:39 -0600

    Completed Tutorials

--- BasicInteractionTutorials/ViewController.swift
+++ BasicInteractionTutorials/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var textMessage: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let label = UILabel()
+//        label.frame = CGRect(x: 125, y: 125, width: 200, height: 80)
+//        label.text = "This is a label"
+//        view.addSubview(label)
+//
+//        let button = UIButton()
+//        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
+//        button.setTitle("Button", for: .normal)
+//        button.setTitleColor(.blue, for: .normal)
+//        view.addSubview(button)
     }
-
-
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textMessage.text?.uppercased()
+    }
 }
-
